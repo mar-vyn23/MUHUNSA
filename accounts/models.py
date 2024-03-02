4a38b6688d219dd833cd9158c2b04f87012eb087
mar-vyn23 <marvovybes23@gmail.com>
2024-03-02 11:06:10 +0300

    ℹ️ Initial release: MUHUNSA Blog Site v1.0 🚀
    This commit marks the official release of version 1.0 of the MUHUNSA Blog Site. It includes the initial set of features, configurations, and the foundation for our blog platform. Let the blogging journey begin! 📝✨

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
+from .managers import UserManager
+
+class User(AbstractUser):
+    # is_active = models.BooleanField(default=False)  # New field for email activation
+    is_author = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to="avatars/", default="avatars/default.jpeg")
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
